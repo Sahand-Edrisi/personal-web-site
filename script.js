01f432d4bd7c9af4e30976c27d09b10f6babbe97
sahand-edc <sahand.edc@gmail.com>
2023-07-21 03:02:56 +0330

    update

--- script.js
+++ script.js
@@ -1,65 +1,95 @@
-document.body.classList.add("light-theme");
 
+// cheange-theme
+
+document.body.classList.add("light-theme");
 document.getElementById("change-theme").addEventListener("click", (e) => {
   if (document.body.classList.contains("light-theme")) {
     document.body.classList.replace("light-theme", "dark-theme");
- 
+   
   } else {
     document.body.classList.replace("dark-theme", "light-theme");
-  
   }
 });
 
-let home = document.getElementById('home')
-let about = document.getElementById('about')
-let portfolio = document.getElementById('portfolio')
-let myBlogs = document.getElementById('myBlogs')
-let contactMe = document.getElementById('contactMe')
+// active-hover-btn
 
-function homeClick(){
-  if (home.style.display === "none") {  
+const activeBtn = document.querySelectorAll(".hover");
+
+console.log(activeBtn);
+
+activeBtn.forEach((active) => {
+  active.addEventListener("click", () => {
+    removeActiveClasses();
+    active.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  activeBtn.forEach((active) => {
+    active.classList.remove("active");
+  });
+}
+
+// cheange-page
+
+const homeClick = document.getElementById("homeClick");
+const home = document.getElementById("home");
+const aboutClick = document.getElementById("aboutClick");
+const about = document.getElementById("about");
+const portfolioClick = document.getElementById("portfolioClick");
+const portfolio = document.getElementById("portfolio");
+const myBlogsClick = document.getElementById("myBlogsClick");
+const myBlogs = document.getElementById("myBlogs");
+const contactMeClick = document.getElementById("contactMeClick");
+const contactMe = document.getElementById("contactMe");
+
+console.log(homeClick);
+
+homeClick.addEventListener("click", () => {
+  if (home.style.display === "none") {
     about.style.display = "none";
     portfolio.style.display = "none";
     myBlogs.style.display = "none";
     contactMe.style.display = "none";
     home.style.display = "block";
   }
-}
-
-function aboutClick(){
-  if (about.style.display === "none") {  
+});
+aboutClick.addEventListener("click", () => {
+  if (about.style.display === "none") {
     home.style.display = "none";
     portfolio.style.display = "none";
     myBlogs.style.display = "none";
     contactMe.style.display = "none";
     about.style.display = "block";
   }
-}
+});
 
-function portfolioClick(){
-  if (portfolio.style.display === "none") {  
+portfolioClick.addEventListener("click", () => {
+  if (portfolio.style.display === "none") {
     home.style.display = "none";
     about.style.display = "none";
     myBlogs.style.display = "none";
     contactMe.style.display = "none";
     portfolio.style.display = "block";
   }
-}
-function myBlogsClick(){
-  if (myBlogs.style.display === "none") {  
+});
+
+myBlogsClick.addEventListener("click", () => {
+  if (myBlogs.style.display === "none") {
     home.style.display = "none";
     portfolio.style.display = "none";
     contactMe.style.display = "none";
     about.style.display = "none";
     myBlogs.style.display = "block";
   }
-}
-function contactMeClick(){
-  if (contactMe.style.display === "none") {  
+});
+
+contactMeClick.addEventListener("click", () => {
+  if (contactMe.style.display === "none") {
     home.style.display = "none";
     portfolio.style.display = "none";
     about.style.display = "none";
     myBlogs.style.display = "none";
     contactMe.style.display = "block";
   }
-}
+});
